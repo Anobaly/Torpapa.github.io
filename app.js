2b044216d1ff094363bb43a6993e291a96b519c2
Anobaly <70094552+Anobaly@users.noreply.github.com>
2020-09-09 17:38:18 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
